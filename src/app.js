8c27c3b8ad4fb4db8a6c452bd3580439bd34ab52
jayantbirla6268 <jayantbirla12345@gmail.com>
2025-05-15 08:44:12 +0530

    create a server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+ const express=require("express");
+ const app=express();
+
+
+ app.use((req,res)=>{
+     res.send("hello from the server");
+
+ });
+ app.listen(3000,()=>  {
+    console.log("server is sucessfully listining on port 3000...");
+
+ });
+ 
