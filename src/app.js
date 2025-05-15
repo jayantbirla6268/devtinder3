4301ae40e84ccf5ada1c734ba344620d4e10dc58
jayantbirla6268 <jayantbirla12345@gmail.com>
2025-05-15 16:20:36 +0530

    exploring the routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
  const app=express();
 
 
- app.use((req,res)=>{
-     res.send("hello from the server");
+  
 
- });
+app.get("/user",(req,res)=>{
+    res.send({first:"jayantbirla",last:"birla"})
+});
+
+app.post("/user",(req,res)=>{
+    //saving data to db
+    res.send("data sucessfully saved to the database");
+});
+//app.use("/test",(req,res)=>{
+    //res.send("hello from the server");
+//});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted sucessfully")
+})
+
+ 
  app.listen(3000,()=>  {
     console.log("server is sucessfully listining on port 3000...");
 
